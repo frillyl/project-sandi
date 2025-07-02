2dd8df00559eeb8f8a643731eeb19a4a663f9261
Frilly Lakumani <frillylakumani.1905@gmail.com>
2025-07-02 07:50:22 +0700

    Update Frontend

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -129,7 +129,7 @@ const fetchUserData = async () => {
   const token = localStorage.getItem('auth_token')
   if (token) {
     try {
-      const response = await axios.get('https://suspected-outputs-telephone-mating.trycloudflare.com/api/user', {
+      const response = await axios.get('https://incomplete-fan-renewal-impossible.trycloudflare.com/api/user', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
